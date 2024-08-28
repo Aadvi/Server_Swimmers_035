cb7c222409bc3f0f485204a4d77cdb7cafc339ed
Aadvi <avs.rasra99@gmail.com>
2024-08-29 02:09:13 +0530

    drag_and_drop_and_toolbox

--- QuickSite/src/App.jsx
+++ QuickSite/src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import grapesjs from "grapesjs";
+import gjsPresetWebpage from "grapesjs-preset-webpage";
+import gjsblocksbasic from "grapesjs-blocks-basic";
+import "./styles/main.scss";
+import grapesjsblocksbootstrap from "grapesjs-blocks-bootstrap4";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [editor, setEditor] = useState(null);
+  useEffect(() => {
+    const editor = grapesjs.init({
+      container: "#editor",
+      plugins: [gjsPresetWebpage, gjsblocksbasic],
+
+      pluginsOpts: {
+        gjsPresetWebpage: {},
+        gjsblocksbasic: {},
+      },
+    });
+    setEditor(editor);
+  }, []);
 
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <h1>Website Builder</h1>
+      <div id="editor"></div>
+    </div>
+  );
 }
 
-export default App
+export default App;
