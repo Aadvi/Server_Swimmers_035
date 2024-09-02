e61cd2b107eb29e0bdca7cb89baa5b5a75807bb5
Aadvi <avs.rasra99@gmail.com>
2024-09-02 08:56:26 +0530

    updated-repo

--- backend/routes/pageRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getPages, createPage } from "../controllers/pageController.js";
-
-const router = express.Router();
-
-router.get("/pages", getPages);
-router.post("/pages", createPage);
-
-export default router;
